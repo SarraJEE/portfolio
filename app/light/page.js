63a476e5a2a5fb4f90f1f49d57c666b9b4402f99
SarraJEE <bgrsarra@gmail.com>
2024-03-04 23:47:35 +0100

    Maj CSS

--- app/light/page.js
+++ app/light/page.js
@@ -4,13 +4,13 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Home from "@/components/Home";
 import Portfolio from "@/components/Portfolio";
-import { TunisContext } from "@/context/context";
+import { PortfolioContext } from "@/context/context";
 import Revealer from "@/layouts/Revealer";
 import PortfolioLayout from "@/layouts/PortfolioLayout";
 import { useContext, useEffect } from "react";
 
 const Page = () => {
-  const { darkToggle, changeColor } = useContext(TunisContext);
+  const { darkToggle, changeColor } = useContext(PortfolioContext);
   useEffect(() => {
     darkToggle();
     changeColor("green");
